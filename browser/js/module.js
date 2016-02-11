85cb1bedaaa13dc8294f80e5bf052b8a4b6b830a
George Holevas <gholevas1@gmail.com>
2016-02-10 20:12:14 -0500

    dropped the # in routes

--- browser/js/module.js
+++ browser/js/module.js
@@ -3,10 +3,13 @@
 var juke = angular.module('juke', ['ui.router'])
 
 
-.config(function($stateProvider,$urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-	$urlRouterProvider.when('', '/albums');
-	$urlRouterProvider.when('/artist/:id', '/artist/:id/albums');
+    $urlRouterProvider
+        .when('', '/albums')
+        .when('/artist/:id', '/artist/:id/albums');
+
+    $locationProvider.html5Mode(true);
 
     $stateProvider
         .state('albumList', {
@@ -33,7 +36,7 @@ var juke = angular.module('juke', ['ui.router'])
             url: '/album/:id',
             templateUrl: 'album.html',
             resolve: {
-                album: function(AlbumFactory,$stateParams) {
+                album: function(AlbumFactory, $stateParams) {
                     return AlbumFactory.fetchById($stateParams.id);
                 }
             },
@@ -43,7 +46,7 @@ var juke = angular.module('juke', ['ui.router'])
             url: '/artist/:id',
             templateUrl: 'artist.html',
             resolve: {
-                artist: function(ArtistFactory,$stateParams) {
+                artist: function(ArtistFactory, $stateParams) {
                     return ArtistFactory.fetchById($stateParams.id);
                 }
             },
